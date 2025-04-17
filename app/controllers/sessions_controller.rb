44c6ccf575caaa82dd1c7cd137faae0675289d21
David Boureau <bdavidxyz@gmail.com>
2025-04-17 13:43:32 +0200

    im lost

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,7 @@
 class SessionsController < ApplicationController
   skip_before_action :authenticate, only: %i[ new create ]
 
+  before_action :find_bot, only: :create
   before_action :set_session, only: :destroy
 
   def index
@@ -14,8 +15,6 @@ class SessionsController < ApplicationController
     if user = User.authenticate_by(email: params[:email], password: params[:password])
       @session = user.sessions.create!
       cookies.signed.permanent[:session_token] = { value: @session.id, httponly: true }
-
-      puts "wooooooooooooooooooooooooooooooooooooooooorks"
 
       redirect_to root_path, notice: "Signed in successfully"
     else
