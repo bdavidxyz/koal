d85d38f8088055b6fba81058a2af733c5a2eff70
David Boureau <bdavidxyz@gmail.com>
2025-04-18 09:59:47 +0200

    added section to default myaccount

--- app/controllers/myaccount_controller.rb
+++ app/controllers/myaccount_controller.rb
@@ -1,8 +1,12 @@
 class MyaccountController < ApplicationController
   def index
+    u = Current.user
+    sessions = u.sessions
+    session = Current.session
     render locals: {
-      email: Current.user.email,
-      session: Current.session
+      email: u.email,
+      session: session,
+      session_number: u.sessions.order(created_at: :desc)
     }
   end
 end
