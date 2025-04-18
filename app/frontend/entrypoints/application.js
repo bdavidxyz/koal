1e94430b5a9e72aa7fa36e234d2a036c43fcec4a
David Boureau <bdavidxyz@gmail.com>
2025-04-18 11:06:44 +0200

    default unpoly config

--- app/frontend/entrypoints/application.js
+++ app/frontend/entrypoints/application.js
@@ -2,34 +2,13 @@ import "unpoly/unpoly.js";
 
 import "../up_compiler/all.js";
 
-// To see this message, add the following to the `<head>` section in your
-// views/layouts/application.html.erb
-//
-//    <%= vite_client_tag %>
-//    <%= vite_javascript_tag 'application' %>
-console.log("Vite ⚡️ Rails");
+// config links for Unpoly
+up.link.config.followSelectors.push("a[href]");
+up.link.config.preloadSelectors.push("a[href]");
 
-// If using a TypeScript entrypoint file:
-//     <%= vite_typescript_tag 'application' %>
-//
-// If you want to use .jsx or .tsx, add the extension:
-//     <%= vite_javascript_tag 'application.jsx' %>
+// config forms for Unpoly
+up.form.config.submitSelectors.push("form");
+up.link.config.instantSelectors.push("a[href]");
 
-console.log(
-  "Visit the guide for more information: ",
-  "https://vite-ruby.netlify.app/guide/rails"
-);
-
-// Example: Load Rails libraries in Vite.
-//
-// import * as Turbo from '@hotwired/turbo'
-// Turbo.start()
-//
-// import ActiveStorage from '@rails/activestorage'
-// ActiveStorage.start()
-//
-// // Import all channels.
-// const channels = import.meta.globEager('./**/*_channel.js')
-
-// Example: Import a stylesheet in app/frontend/index.css
-// import '~/index.css'
+// disable/enable logs for Unpoly
+up.log.disable();
