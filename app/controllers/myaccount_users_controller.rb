92e5d8dbe0845e219307abd49f9bedaf81c51824
David Boureau <bdavidxyz@gmail.com>
2025-04-23 15:26:05 +0200

    myaccount_users_index

--- app/controllers/myaccount_users_controller.rb
+++ app/controllers/myaccount_users_controller.rb
@@ -6,22 +6,28 @@ class MyaccountUsersController < ApplicationController
   # @route GET /myaccount/users (myaccount_users)
   def index
   end
+
   # @route GET /myaccount/users/:id (myaccount_user)
   def show
   end
+
   # @route GET /myaccount/users/new (new_myaccount_user)
   def new
   end
+
   # @route GET /myaccount/users/:id/edit (edit_myaccount_user)
   def edit
   end
+
   # @route POST /myaccount/users (myaccount_users)
   def create
   end
+
   # @route PATCH /myaccount/users/:id (myaccount_user)
   # @route PUT /myaccount/users/:id (myaccount_user)
   def update
   end
+
   # @route DELETE /myaccount/users/:id (myaccount_user)
   def destroy
   end
