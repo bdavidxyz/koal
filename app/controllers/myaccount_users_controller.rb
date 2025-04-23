b4d6e264a00ede4ca8ada37f573e37326b69b8fd
David Boureau <bdavidxyz@gmail.com>
2025-04-23 13:51:50 +0200

    better route annotation

--- app/controllers/myaccount_users_controller.rb
+++ app/controllers/myaccount_users_controller.rb
@@ -3,25 +3,26 @@ class MyaccountUsersController < ApplicationController
 
   grant_access roles: :superadmin
 
+  # @route GET /myaccount/users (myaccount_users)
   def index
   end
-  # @route GET /myaccount/users (myaccount_users)
+  # @route GET /myaccount/users/:id (myaccount_user)
   def show
   end
-  # @route GET /myaccount/users/new (new_myaccount_users)
+  # @route GET /myaccount/users/new (new_myaccount_user)
   def new
   end
-  # @route GET /myaccount/users/edit (edit_myaccount_users)
+  # @route GET /myaccount/users/:id/edit (edit_myaccount_user)
   def edit
   end
   # @route POST /myaccount/users (myaccount_users)
   def create
   end
-  # @route PATCH /myaccount/users (myaccount_users)
-  # @route PUT /myaccount/users (myaccount_users)
+  # @route PATCH /myaccount/users/:id (myaccount_user)
+  # @route PUT /myaccount/users/:id (myaccount_user)
   def update
   end
-  # @route DELETE /myaccount/users (myaccount_users)
+  # @route DELETE /myaccount/users/:id (myaccount_user)
   def destroy
   end
 end
