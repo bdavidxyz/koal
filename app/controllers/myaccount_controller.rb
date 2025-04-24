2278c674db35be46faae83419b382ce06286afd3
David Boureau <bdavidxyz@gmail.com>
2025-04-24 11:18:26 +0200

    grant access per action

--- app/controllers/myaccount_controller.rb
+++ app/controllers/myaccount_controller.rb
@@ -1,33 +1,51 @@
 class MyaccountController < ApplicationController
-  grant_access roles: :member
 
+  grant_access roles: :member, action: :index
   # @route GET /myaccount (myaccount)
   def index
   end
+
+  grant_access roles: :member, action: :sessions
   # @route GET /myaccount/sessions (myaccount_sessions)
   def sessions
     render locals: {
       sessions: Current.user.sessions.order(created_at: :desc)
     }
   end
+
+  grant_access roles: :member, action: :email
   # @route GET /myaccount/email (myaccount_email)
   def email
     render locals: {
       user: Current.user
     }
   end
+
+  grant_access roles: :member, action: :password
   # @route GET /myaccount/password (myaccount_password)
   def password
     render locals: {
       user: Current.user
     }
   end
+
+  grant_access roles: :member, action: :profile
   # @route GET /myaccount/profile (myaccount_profile)
   def profile
     render locals: {
       user: Current.user
     }
   end
+
+  grant_access roles: :member, action: :billing
+  def billing
+  end
+
+  grant_access roles: :member, action: :danger
+  def danger
+  end
+
+  grant_access roles: :member, action: :destroy_account
   # @route DELETE /myaccount/destroy (myaccount_destroy)
   def destroy_account
     user = Current.user
