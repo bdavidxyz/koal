ba8fc4c809a4cdcf512e8cfcb2e6a0cedbafa9af
David Boureau <bdavidxyz@gmail.com>
2025-04-26 20:02:56 +0200

    ability to sort

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,4 +8,12 @@ module ApplicationHelper
   def current_session
     Current.session
   end
+  def build_href_to_sort(sort_by, query_params)
+    direction = query_params["direction"]
+    query_copy = query_params.map { |k, v| [ k, v ] }.to_h
+    query_copy["sort"] = sort_by
+    query_copy["direction"] = direction == "asc" ? "desc" : "asc"
+    query_string = query_copy.map { |key, value| "#{key}=#{value}" }.join("&")
+    query_string
+  end
 end
