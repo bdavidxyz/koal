a129f5605e6e8648441774d646677e3c46bc6751
David Boureau <bdavidxyz@gmail.com>
2025-06-23 14:06:55 +0200

    two green  tests

--- test/controllers/passwords_controller_test.rb
+++ test/controllers/passwords_controller_test.rb
@@ -12,10 +12,10 @@ class PasswordsControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
-  # test "should update password" do
-  #   patch password_url, params: { password_challenge: "Secret1*3*5*", password: "Secret6*4*2*", password_confirmation: "Secret6*4*2*" }
-  #   assert_redirected_to myaccount_path
-  # end
+  test "should update password" do
+    patch password_path, params: { password_challenge: "Secret1*3*5*", password: "Secret6*4*2*", password_confirmation: "Secret6*4*2*" }
+    assert_redirected_to myaccount_path
+  end
 
   # test "should not update password with wrong password challenge" do
   #   patch password_url, params: { password_challenge: "SecretWrong1*3", password: "Secret6*4*2*", password_confirmation: "Secret6*4*2*" }
