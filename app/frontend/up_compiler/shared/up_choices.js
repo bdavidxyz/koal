4746bda6efc1301f12c3f7e8c53d7f7b5312864d
David Boureau <bdavidxyz@gmail.com>
2025-08-27 13:00:30 +0200

    restyled tags, amazing

--- app/frontend/up_compiler/shared/up_choices.js
+++ app/frontend/up_compiler/shared/up_choices.js
@@ -7,6 +7,40 @@ up.compiler(".up_choices", function (element, data) {
     placeholder: true,
     placeholderValue: "Blog tags",
     removeItemButton: true,
+    classNames: {
+      containerOuter: ["choices", "w-full", "!mt-2"],
+      containerInner: [
+        "choices__inner",
+        "!bg-white",
+        "!pt-2",
+        "!pb-0",
+        "!border-blue-200",
+        "hover:!border-blue-300",
+        "focus-within:!border-blue-400",
+        "focus-within:!shadow-sm",
+        "focus-within:!shadow-blue-100",
+        "!rounded-lg",
+      ],
+      item: [
+        "choices__item",
+        "!text-blue-800",
+        "!bg-blue-50",
+        "!border",
+        "!border-blue-200",
+        "hover:!bg-blue-100",
+        "hover:!border-blue-300",
+        "!rounded-md",
+        "!transition-colors",
+      ],
+      button: [
+        "choices__button",
+        "!text-blue-600",
+        "hover:!text-blue-800",
+        "!border-none",
+        "!bg-[url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMjEiIGhlaWdodD0iMjEiIHZpZXdCb3g9IjAgMCAyMSAyMSIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj48ZyBmaWxsPSIjMjU2M2ViIiBmaWxsLXJ1bGU9ImV2ZW5vZGQiPjxwYXRoIGQ9Ik0yLjU5Mi4wNDRsMTguMzY0IDE4LjM2NC0yLjU0OCAyLjU0OEwuMDQ0IDIuNTkyeiIvPjxwYXRoIGQ9Ik0wIDE4LjM2NEwxOC4zNjQgMGwyLjU0OCAyLjU0OEwyLjU0OCAyMC45MTJ6Ii8+PC9nPjwvc3ZnPg==)]",
+        "!transition-colors",
+      ],
+    },
     choices: data.allItems.map((e) => {
       let res = { value: e.name, label: e.name };
       if (data?.selItems?.length > 0) {
