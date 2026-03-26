a5abda8100b24ab081c5a03537bb530875f255a7
David Boureau <bdavidxyz@gmail.com>
2026-03-26 10:11:55 +0100

    feat: versioned migrations

--- db/errors_schema.rb
+++ db/errors_schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 0) do
+ActiveRecord::Schema[8.1].define(version: 2025_05_01_000001) do
   create_table "solid_errors", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.text "exception_class", null: false
