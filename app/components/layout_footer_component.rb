5767035f0e0f31fde5299f71850fd43928ec97b6
David Boureau <bdavidxyz@gmail.com>
2026-04-02 16:15:07 +0200

    hide footer in dashboard

--- app/components/layout_footer_component.rb
+++ app/components/layout_footer_component.rb
@@ -1,10 +1,13 @@
 class LayoutFooterComponent < ViewComponent::Base
-  def initialize(controller_name:, action_name:)
+  def initialize(controller_name:, action_name:, request_path:)
     @controller_name = controller_name
     @action_name = action_name
+    @request_path = request_path
   end
 
   def render?
+    return false if @request_path.start_with?("/myaccount")
+
     !(%w[sessions registrations].include?(@controller_name) && @action_name == "new")
   end
 end
