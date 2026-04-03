5e3eb65e83ada1c0b03c40f9f1671d8ce2e157ef
David Boureau <bdavidxyz@gmail.com>
2026-04-03 09:33:08 +0200

    better flash icon

--- app/frontend/custom_js/toastify.js
+++ app/frontend/custom_js/toastify.js
@@ -231,7 +231,7 @@ class Toastify {
     let iconElement = document.createElement("div");
     iconElement.className = "toast-icon";
     iconElement.innerHTML =
-      "<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><circle cx='12' cy='12' r='10'/><line x1='12' y1='8' x2='12' y2='12'/><line x1='12' y1='16' x2='12.01' y2='16'/></svg>";
+      "<svg xmlns='http://www.w3.org/2000/svg' width='20' height='20' viewBox='0 0 24 24' fill='none' stroke='currentColor' stroke-width='2' stroke-linecap='round' stroke-linejoin='round'><circle cx='12' cy='12' r='10'/><path d='M12 16v-4'/><path d='M12 8h.01'/></svg>";
     divElement.appendChild(iconElement);
 
     // Adding the toast message/node inside a body container
